73e5f971e4a499f7c5f9198b6256424db4d68fa3
MicaBalza <mica.balza@gmail.com>
2020-08-20 19:36:29 -0300

    Desafio Clase 1

---
+++ Clase01/clase01.js
@@ -0,0 +1,9 @@
+nombre = prompt("Cuál es tu nombre?");
+apellido = prompt("Y tu apellido?");
+
+alert("Bienvenido " + nombre + " " + apellido + "!");
+
+num2 = 10;
+num1 = parseInt(prompt("Elegí un número del 1 al 10"));
+
+console.log(num1 + num2);
